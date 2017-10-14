c801b65e241fea33575c94a05745370ec595d1fb
Fernando Henrique <fernandodojo@gmail.com>
2017-10-14 17:50:37 -0300

    Exercícios Monitoria

--- Exercícios Monitoria/13-10-17/1ª.lua
+++ Exercícios Monitoria/13-10-17/1ª.lua
@@ -1,10 +1,14 @@
 function checaToqueR(posxT, posyT, posxR, posyR)
-	base = posxR * 100
-	altura = posyR * 30
-	--retangulo = base * altura
+	
 
-	if posxT > 0 and posxT <= base then
-		if posyT> 0 and posyT <= altura then
+
+	base = posxR +100
+	altura = posyR - 30
+		
+
+
+	if posxT >= posxR and posxT <= base then
+		if posyT<= posyR and posyT >= altura then
 			return true
 		else
 			return false
@@ -19,9 +23,15 @@ end
 	a = io.read("*n")
 	print("Digite a posição y do Ponto de Toque")
 	b = io.read("*n")
-	print("Digite a posição x do Retangulo")
+	print("Digite a posição x de origem do Retangulo")
 	c = io.read("*n")
-	print("Digite a posição y do Retangulo")
+	print("Digite a posição y de origem do Retangulo")
 	d = io.read("*n")
+
+
+
+
+
+
 
 	print(checaToqueR(a,b,c,d))
